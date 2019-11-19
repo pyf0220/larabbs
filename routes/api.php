23da8fc26169c5be3ef8a34043c103764e359405
pyf <792722527@qq.com>
2019-11-19 18:50:31 +0800

    用户注册

--- routes/api.php
+++ routes/api.php
@@ -46,3 +46,13 @@ Route::prefix('v1')
 
             });
     });
+
+// 图片验证码
+Route::post('captchas', 'CaptchasController@store')
+    ->name('captchas.store');
+// 短信验证码
+Route::post('verificationCodes', 'VerificationCodesController@store')
+    ->name('verificationCodes.store');
+// 用户注册
+Route::post('users', 'UsersController@store')
+    ->name('users.store');
